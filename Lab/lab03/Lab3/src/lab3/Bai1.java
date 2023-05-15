78c4e098a653744a4d62e5bb89254c2302cf9302
dquangv <dquangvu7998@gmail.com>
2023-05-15 22:32:54 +0700

    Add lab3.1 and lab 3.2

---
+++ Lab/lab03/Lab3/src/lab3/Bai1.java
@@ -0,0 +1,29 @@
+package lab3;
+
+import java.util.Scanner;
+
+public class Bai1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        boolean check = true;
+
+        System.out.println("Enter a number to check:");
+        int num = sc.nextInt();
+
+        if (num == 1) {
+            System.out.println("This number is not prime");
+        } else {
+            for (int index = 2; index <= num / 2; index++) {
+                if ((num % index == 0)) {
+                    check = false;
+                    break;
+                }
+            }
+
+            String prime = check == true ? "This number is prime" : "This number is not prime";
+            System.out.println(prime);
+        }
+    }
+}
