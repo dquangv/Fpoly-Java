2d26c094695e1ea3a8ffc7f6e09456c4ca115db2
dquangv <dquangvu7998@gmail.com>
2023-05-18 23:00:14 +0700

    Add assignment

---
+++ Asm/asm/src/asm/Asm.java
@@ -0,0 +1,54 @@
+package asm;
+
+import java.util.Scanner;
+
+public class Asm {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        do {
+            System.out.println("*\t*\t*\t*\t*");
+            System.out.println("*\t       MENU\t\t*");
+            System.out.println("* 1.\t\t\t\t*");
+            System.out.println("* 2.\t\t\t\t*");
+            System.out.println("* 3.\t\t\t\t*");
+            System.out.println("* 4.\t\t\t\t*");
+            System.out.println("* 5.\t\t\t\t*");
+            System.out.println("* 6.\t\t\t\t*");
+            System.out.println("* 7.\t\t\t\t*");
+            System.out.println("* 8.\t\t\t\t*");
+            System.out.println("* 9.\t\t\t\t*");
+            System.out.println("* 10.\t\t\t\t*");
+            System.out.println("*\t*\t*\t*\t*");
+            System.out.print("Chon chuc nang: ");
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
+
+                    System.exit(0);
+            }
+        } while (true);
+    }
+
+}
