91187f1968c2cd27d72c823fdb4a69ff0b10fd47
dquangv <dquangvu7998@gmail.com>
2023-05-19 13:13:03 +0700

    Update asm (menu)

--- Asm/asm/src/asm/Asm.java
+++ Asm/asm/src/asm/Asm.java
@@ -5,47 +5,57 @@ import java.util.Scanner;
 public class Asm {
 
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner(System.in);
-        
+
         do {
-            System.out.println("*\t*\t*\t*\t*");
-            System.out.println("*\t       MENU\t\t*");
-            System.out.println("* 1.\t\t\t\t*");
-            System.out.println("* 2.\t\t\t\t*");
-            System.out.println("* 3.\t\t\t\t*");
-            System.out.println("* 4.\t\t\t\t*");
-            System.out.println("* 5.\t\t\t\t*");
-            System.out.println("* 6.\t\t\t\t*");
-            System.out.println("* 7.\t\t\t\t*");
-            System.out.println("* 8.\t\t\t\t*");
-            System.out.println("* 9.\t\t\t\t*");
-            System.out.println("* 10.\t\t\t\t*");
-            System.out.println("*\t*\t*\t*\t*");
+            System.out.println("*-------------------------------------------------------------------------------*");
+            System.out.println("*\t\t\t\t\tMENU\t\t\t\t\t*");
+            System.out.println("*\t 1. \tNhap danh sach nhan vien tu ban phim\t\t\t\t*");
+            System.out.println("*\t 2. \tXuat danh sach nhan vien ra ban phim\t\t\t\t*");
+            System.out.println("*\t 3. \tTim va hien thi nhan vien theo ma nhap tu ban phim\t\t*");
+            System.out.println("*\t 4. \tXoa nhan vien theo ma nhap tu ban phim\t\t\t\t*");
+            System.out.println("*\t 5. \tCap nhat thong tin nhan vien theo ma nhap tu ban phim\t\t*");
+            System.out.println("*\t 6. \tTim cac nhan vien theo khoang luong nhap tu ban phim\t\t*");
+            System.out.println("*\t 7. \tSap xep nhan vien theo ho va ten\t\t\t\t*");
+            System.out.println("*\t 8. \tSap xep nhan vien theo thu nhap\t\t\t\t\t*");
+            System.out.println("*\t 9. \tXuat 5 nhan vien co thu nhap cao nhat\t\t\t\t*");
+            System.out.println("*\t 10. \tKet thuc chuong trinh\t\t\t\t\t\t*");
+            System.out.println("*-------------------------------------------------------------------------------*");
+
             System.out.print("Chon chuc nang: ");
             int choice = sc.nextInt();
 
             switch (choice) {
                 case 1:
+                    System.out.println("NHAP DANH SACH NHAN VIEN TU BAN PHIM");
                     break;
                 case 2:
+                    System.out.println("XUAT DANH SACH NHAN VIEN RA MAN HINH");
                     break;
                 case 3:
+                    System.out.println("TIM VA HIEN THI NHAN VIEN THEO MA NHAP TU BAN PHIM");
                     break;
                 case 4:
+                    System.out.println("XOA NHAN VIEN THEO MA NHAP TU BAN PHIM");
                     break;
                 case 5:
+                    System.out.println("CAP NHAT THONG TIN NHAN VIEN THEO MA NHAP TU BAN PHIM");
                     break;
                 case 6:
+                    System.out.println("TIM CAC NHAN VIEN THEO KHOANG LUONG NHAP TU BAN PHIM");
                     break;
                 case 7:
+                    System.out.println("SAP XEP NHAN VIEN THEO HO VA TEN");
                     break;
                 case 8:
+                    System.out.println("SAP XEP NHAN VIEN THEO THU NHAP");
                     break;
                 case 9:
+                    System.out.println("XUAT 5 NHAN VIEN CO THU NHAP CAO NHAT");
                     break;
-                case 10:
-
+                default:
+                    System.out.println("Cam on da su dung dich vu!");
                     System.exit(0);
             }
         } while (true);
