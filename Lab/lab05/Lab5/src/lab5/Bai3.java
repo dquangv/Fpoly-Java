fc7d291de56e73bce5d00b573b8a62e3db72cac7
dquangv <dquangvu7998@gmail.com>
2023-05-29 21:00:37 +0700

    Update lab 5.3

--- Lab/lab05/Lab5/src/lab5/Bai3.java
+++ Lab/lab05/Lab5/src/lab5/Bai3.java
@@ -1,14 +1,16 @@
 package lab5;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
 
 public class Bai3 {
 
-    public class SanPham {
+    public static class SanPham implements Comparator<SanPham> {
 
-        String tenSP;
-        double giaSP;
+        private String tenSP;
+        private double giaSP;
 
         public void SanPham(String tenSP, double giaSP) {
             this.tenSP = tenSP;
@@ -30,31 +32,42 @@ public class Bai3 {
         public void setGiaSP(double giaSP) {
             this.giaSP = giaSP;
         }
+
+        @Override
+        public int compare(SanPham o1, SanPham o2) {
+            return Double.compare(o2.giaSP, o1.giaSP);
+        }
+
     }
 
-    public class dsSP {
+    public static class dsSP {
+        
+        ArrayList<SanPham> dssp = new ArrayList();
 
         public void menu() {
             System.out.println("\t\tMENU");
-            System.out.println("1. Nhap danh sach ho va ten");
-            System.out.println("2. Xuat danh sach ho va ten");
-            System.out.println("3. Xuat danh sach ngau nhien");
-            System.out.println("4. Sap xep giam dan va xuat danh sach");
-            System.out.println("5. Tim va xoa ho ten nhap tu ban phim");
-            System.out.println("6. Ket thuc");
-            System.out.print("Ban muon (1-6): ");
+            System.out.println("1. Nhap danh sach san pham");
+            System.out.println("2. Sap xep giam dan va xuat danh sach");
+            System.out.println("3. Tim va xoa ho ten nhap tu ban phim");
+            System.out.println("4. Xuat gia trung binh cua cac san pham");
+            System.out.println("5. Ket thuc");
+            System.out.print("Ban muon (1-5): ");
         }
 
-        public void nhapSP(ArrayList<SanPham> dssp) {
+        public void nhapSP() {
             Scanner sc = new Scanner(System.in);
 
             while (true) {
                 System.out.print("Ten san pham: ");
-                
-                System.out.println("Gia san pham: ");
-                
+                String ten = sc.nextLine();
+                System.out.print("Gia san pham: ");
+                double gia = Double.parseDouble(sc.nextLine());
 
-                System.out.println("Nhap nua hong? (Y/N): ");
+                SanPham sp = new SanPham();
+                sp.setTenSP(ten);
+                sp.setGiaSP(gia);
+                dssp.add(sp);
+                System.out.print("Nhap nua hong? (Y/N): ");
                 String check = sc.nextLine();
                 if (check.equalsIgnoreCase("No") || check.equalsIgnoreCase("N")) {
                     break;
@@ -62,22 +75,81 @@ public class Bai3 {
             }
         }
 
-        public void xuatSP(ArrayList<Object> dssp) {
-            System.out.println("Danh sach san pham");
+
+        public void sapXepDS() {
+            Collections.sort(dssp, new SanPham());
             for (int i = 0; i < dssp.size(); i++) {
-                System.out.print(dssp.get(i));
+                System.out.println("San pham " + (i + 1));
+                System.out.println("Ten san pham: " + dssp.get(i).getTenSP());
+                System.out.println("Gia san pham: " + dssp.get(i).getGiaSP() + "\n");
             }
         }
-
+        
+        public void timVaXoaDS() {
+            Scanner sc = new Scanner(System.in);
+            ArrayList<String> dsTenXoa = new ArrayList<>();
+            
+            while(true) {
+                System.out.print("Ten san pham muon xoa: ");
+                String tenXoa = sc.nextLine();
+                
+                dsTenXoa.add(tenXoa);
+                System.out.print("Nhap nua hong? (Y/N): ");
+                String check = sc.nextLine();
+                if (check.equalsIgnoreCase("No") || check.equalsIgnoreCase("N")) {
+                    break;
+                }
+            }
+            
+            dssp.removeIf(sp -> dsTenXoa.contains(sp.getTenSP()));
+            for (int i = 0; i < dssp.size(); i++) {
+                System.out.println("San pham " + (i + 1));
+                System.out.println("Ten san pham: " + dssp.get(i).getTenSP());
+                System.out.println("Gia san pham: " + dssp.get(i).getGiaSP() + "\n");
+            }
+        }
+        
+        public double tinhGiaTrungBinh() {
+            double tongGia = 0;
+            for (SanPham sp: dssp) {
+                tongGia += sp.getGiaSP();
+            }
+            
+            return tongGia/dssp.size();
+        }
     }
-    
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        dsSP ds = new dsSP();
         
-        ArrayList<SanPham> dssp = new ArrayList<>();
-        SanPham sp = new SanPham();
-        
-        
+        do {
+            ds.menu();
+            int choice = Integer.parseInt(sc.nextLine());
+            switch (choice) {
+                case 1:
+                   ds.nhapSP();
+                   break;
+                case 2:
+                    ds.sapXepDS();
+                    break;
+                case 3:
+                   ds.timVaXoaDS();
+                   break;
+                case 4:
+                    System.out.println("Gia trung binh cua cac san pham: "+ds.tinhGiaTrungBinh());
+                default:
+                    System.exit(0);
+            }
+            
+            System.out.print("Su dung nua hong? (yes/no): ");
+            String answer = sc.nextLine();
+            if (answer.equalsIgnoreCase("No") || answer.equalsIgnoreCase("N")) {
+                break;
+            }
+        } while (true);
     }
-    
+
 }
+
