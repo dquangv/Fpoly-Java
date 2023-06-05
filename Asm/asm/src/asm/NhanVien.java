3590d7e49856e2868ea238631ec381189396c070
dquangv <dquangvu7998@gmail.com>
2023-06-05 16:13:30 +0700

    Update asm

--- Asm/asm/src/asm/NhanVien.java
+++ Asm/asm/src/asm/NhanVien.java
@@ -43,7 +43,24 @@ public class NhanVien {
         this.luong = luong;
     }
 
-    public void nhapThongTin(ArrayList<NhanVien> dsNV) {
+    public static void menu() {
+        System.out.println("*-------------------------------------------------------------------------------*");
+        System.out.println("*\t\t\t\t\tMENU\t\t\t\t\t*");
+        System.out.println("*\t 1. \tNhap danh sach nhan vien tu ban phim\t\t\t\t*");
+        System.out.println("*\t 2. \tXuat danh sach nhan vien ra ban phim\t\t\t\t*");
+        System.out.println("*\t 3. \tTim va hien thi nhan vien theo ma nhap tu ban phim\t\t*");
+        System.out.println("*\t 4. \tXoa nhan vien theo ma nhap tu ban phim\t\t\t\t*");
+        System.out.println("*\t 5. \tCap nhat thong tin nhan vien theo ma nhap tu ban phim\t\t*");
+        System.out.println("*\t 6. \tTim cac nhan vien theo khoang luong nhap tu ban phim\t\t*");
+        System.out.println("*\t 7. \tSap xep nhan vien theo ho va ten\t\t\t\t*");
+        System.out.println("*\t 8. \tSap xep nhan vien theo thu nhap\t\t\t\t\t*");
+        System.out.println("*\t 9. \tXuat 5 nhan vien co thu nhap cao nhat\t\t\t\t*");
+        System.out.println("*\t 10. \tKet thuc chuong trinh\t\t\t\t\t\t*");
+        System.out.println("*-------------------------------------------------------------------------------*");
+    }
+
+    public static void nhapThongTin(ArrayList<NhanVien> dsNV) {
+        System.out.println("NHAP DANH SACH NHAN VIEN TU BAN PHIM");
         Scanner sc = new Scanner(System.in);
         while (true) {
             NhanVien nhanVien = new NhanVien();
@@ -56,7 +73,7 @@ public class NhanVien {
             nhanVien.setThuNhap(Double.parseDouble(sc.nextLine()));
 
             dsNV.add(nhanVien);
-            
+
             System.out.print("Nhap nua hong? (Y/N): ");
             String check = sc.nextLine();
             if (check.equalsIgnoreCase("No") || check.equalsIgnoreCase("N")) {
@@ -64,51 +81,95 @@ public class NhanVien {
             }
         }
     }
-    
-    public void xuatThongTin(ArrayList<NhanVien> dsNV) {
+
+    public static void xuatThongTin(ArrayList<NhanVien> dsNV) {
+        System.out.println("XUAT DANH SACH NHAN VIEN RA MAN HINH");
         for (int i = 0; i < dsNV.size(); i++) {
-            System.out.println("\nMa nhan vien: "+dsNV.get(i).getMaNV());
-            System.out.println("Ho ten: "+dsNV.get(i).getHoTen());
-            System.out.println("Luong: "+dsNV.get(i).getThuNhap());
-            System.out.println("Thue thu nhap: "+dsNV.get(i).getThueTN());
+            System.out.println("\nMa nhan vien: " + dsNV.get(i).getMaNV());
+            System.out.println("Ho ten: " + dsNV.get(i).getHoTen());
+            System.out.println("Luong: " + dsNV.get(i).getThuNhap());
+            System.out.println("Thue thu nhap: " + dsNV.get(i).getThueTN());
+        }
+    }
+
+    public static void timThongTin(ArrayList<NhanVien> dsNV) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("TIM VA HIEN THI NHAN VIEN THEO THONG TIN NHAP TU BAN PHIM");
+
+        System.out.print("Nhap thong tin nhan vien ban muon tim: ");
+        String thongTin = sc.nextLine();
+        int countCheck = 0;
+
+        for (int i = 0; i < dsNV.size(); i++) {
+            if (dsNV.get(i).getMaNV().equalsIgnoreCase(thongTin) || dsNV.get(i).getHoTen().equalsIgnoreCase(thongTin) || String.format("%.0f", dsNV.get(i).getThuNhap()).equalsIgnoreCase(thongTin)) {
+                System.out.println("\nMa nhan vien: " + dsNV.get(i).getMaNV());
+                System.out.println("Ho ten: " + dsNV.get(i).getHoTen());
+                System.out.println("Luong: " + dsNV.get(i).getThuNhap());
+                System.out.println("Thue thu nhap: " + dsNV.get(i).getThueTN());
+                countCheck++;
+            }
+        }
+
+        if (countCheck != 0) {
+            System.out.println("Khong tim thay thong tin nhan vien thoa du lieu da nhap");
+        }
+    }
+
+    public static void xoaThongTin(ArrayList<NhanVien> dsNV) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> dsTenXoa = new ArrayList<>();
+        System.out.println("XOA NHAN VIEN THEO MA NHAP TU BAN PHIM");
+        while (true) {
+            System.out.print("Nhap thong tin nhan vien ban muon xoa: ");
+            String thongTinXoa = sc.nextLine();
+
+            dsTenXoa.add(thongTinXoa);
+            System.out.print("Muon xoa them hong? (Y/N): ");
+            String check = sc.nextLine();
+            if (check.equalsIgnoreCase("No") || check.equalsIgnoreCase("N")) {
+                break;
+            }
+        }
+        for (int i = 0; i < dsNV.size(); i++) {
+            for (int j = 0; i < dsTenXoa.size(); j++) {
+                if (dsNV.get(i).getMaNV().equalsIgnoreCase(dsTenXoa.get(j)) || dsNV.get(i).getHoTen().equalsIgnoreCase(dsTenXoa.get(j)) || String.format("%.0f", dsNV.get(i).getThuNhap()).equalsIgnoreCase(dsTenXoa.get(j))) {
+                    dsNV.removeAll(dsTenXoa);
+                }
+
+            }
+        }
+
+        System.out.println("Danh sach sau khi xoa");
+        for (int i = 0; i < dsNV.size(); i++) {
+            System.out.println("\nMa nhan vien: " + dsNV.get(i).getMaNV());
+            System.out.println("Ho ten: " + dsNV.get(i).getHoTen());
+            System.out.println("Luong: " + dsNV.get(i).getThuNhap());
+            System.out.println("Thue thu nhap: " + dsNV.get(i).getThueTN());
         }
     }
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
+        ArrayList<NhanVien> dsNV = new ArrayList<>();
         do {
-            System.out.println("*-------------------------------------------------------------------------------*");
-            System.out.println("*\t\t\t\t\tMENU\t\t\t\t\t*");
-            System.out.println("*\t 1. \tNhap danh sach nhan vien tu ban phim\t\t\t\t*");
-            System.out.println("*\t 2. \tXuat danh sach nhan vien ra ban phim\t\t\t\t*");
-            System.out.println("*\t 3. \tTim va hien thi nhan vien theo ma nhap tu ban phim\t\t*");
-            System.out.println("*\t 4. \tXoa nhan vien theo ma nhap tu ban phim\t\t\t\t*");
-            System.out.println("*\t 5. \tCap nhat thong tin nhan vien theo ma nhap tu ban phim\t\t*");
-            System.out.println("*\t 6. \tTim cac nhan vien theo khoang luong nhap tu ban phim\t\t*");
-            System.out.println("*\t 7. \tSap xep nhan vien theo ho va ten\t\t\t\t*");
-            System.out.println("*\t 8. \tSap xep nhan vien theo thu nhap\t\t\t\t\t*");
-            System.out.println("*\t 9. \tXuat 5 nhan vien co thu nhap cao nhat\t\t\t\t*");
-            System.out.println("*\t 10. \tKet thuc chuong trinh\t\t\t\t\t\t*");
-            System.out.println("*-------------------------------------------------------------------------------*");
-
+            menu();
             try {
                 System.out.print("Chon chuc nang: ");
                 int choice = Integer.parseInt(sc.nextLine());
 
                 switch (choice) {
                     case 1:
-                        System.out.println("NHAP DANH SACH NHAN VIEN TU BAN PHIM");
+                        nhapThongTin(dsNV);
                         break;
                     case 2:
-                        System.out.println("XUAT DANH SACH NHAN VIEN RA MAN HINH");
+                        xuatThongTin(dsNV);
                         break;
                     case 3:
-                        System.out.println("TIM VA HIEN THI NHAN VIEN THEO MA NHAP TU BAN PHIM");
+                        timThongTin(dsNV);
                         break;
                     case 4:
-                        System.out.println("XOA NHAN VIEN THEO MA NHAP TU BAN PHIM");
+                        xoaThongTin(dsNV);
                         break;
                     case 5:
                         System.out.println("CAP NHAT THONG TIN NHAN VIEN THEO MA NHAP TU BAN PHIM");
@@ -127,9 +188,10 @@ public class NhanVien {
                         break;
                     case 10:
                         System.out.println("Cam on da su dung dich vu!");
+                        System.exit(0);
                     default:
                         System.out.println("Nhap dung dum cai ma!");
-                        System.exit(0);
+                        break;
                 }
             } catch (Exception e) {
                 System.out.println("nhap so tu 1 den 10 dum cai!");
